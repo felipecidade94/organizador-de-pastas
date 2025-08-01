be3344122544f41e31686f427e6196d636c068b3
felipecidade94 <felipecidade94@gmail.com>
2025-08-01 17:13:30 -0300

    Atualização do código - não sobrescreve os arquivos

--- organizador.py
+++ organizador.py
@@ -7,31 +7,38 @@ def criar_pastas(caminho):
    except FileNotFoundError:
       print(f"Pasta '{caminho}' não encontrada.")
       return
-
+   os.makedirs(f'{caminho}/src', exist_ok=True)
    for arquivo in arquivos:
       caminho_arquivo = os.path.join(caminho, arquivo)
       if os.path.isfile(caminho_arquivo):
          extensao = Path(caminho_arquivo).suffix
          print(f"Arquivo: {arquivo} | Extensão: {extensao}")
-         pasta = f'{caminho}/{extensao[1:]}'
-         os.makedirs(pasta, exist_ok=True)
+         os.makedirs(f'{caminho}/src/{extensao[1:]}', exist_ok=True)
 
-def mover(caminho):
+def mover_src(caminho):
    try:
       arquivos = os.listdir(caminho)
    except FileNotFoundError:
       print(f"Pasta '{caminho}' não encontrada.")
       return
-
    for arquivo in arquivos:
       caminho_arquivo = os.path.join(caminho, arquivo)
       if os.path.isfile(caminho_arquivo):
          extensao = Path(caminho_arquivo).suffix
          print(f"Arquivo: {arquivo} | Extensão: {extensao}")
-         pasta = f'{caminho}/{extensao[1:]}'
-         shutil.move(caminho_arquivo, f'{pasta}/{arquivo}')
-         
+         destino = Path(f'{caminho}/src/{extensao[1:]}/{arquivo}')
+         if not os.path.exists(destino):
+            shutil.move(caminho_arquivo, destino)
+         else:
+            nome, extensao = os.path.splitext(destino)
+            contador = 1
+            novo_destino = f'{nome}_{contador}{extensao}'
+            while os.path.exists(novo_destino):
+               contador += 1
+               novo_destino = f'{nome}_{contador}{extensao}'
+            shutil.move(caminho_arquivo, novo_destino)
+
 if __name__ == "__main__":
    pasta_alvo = "./testes"  # Crie essa pasta e coloque arquivos lá para testar
    criar_pastas(pasta_alvo)
-   mover(pasta_alvo)
+   mover_src(pasta_alvo)
