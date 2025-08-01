8a37e9fff592539031dcca9f973f30250487688e
felipecidade94 <felipecidade94@gmail.com>
2025-08-01 15:38:18 -0300

    Código funcionando

--- listas_arquivos.py
+++ listas_arquivos.py
@@ -1,6 +1,7 @@
 import os
-
-def listar_arquivos_e_extensoes(caminho):
+import shutil
+from pathlib import Path
+def criar_pastas(caminho):
    try:
       arquivos = os.listdir(caminho)
    except FileNotFoundError:
@@ -10,11 +11,27 @@ def listar_arquivos_e_extensoes(caminho):
    for arquivo in arquivos:
       caminho_arquivo = os.path.join(caminho, arquivo)
       if os.path.isfile(caminho_arquivo):
-         _, extensao = os.path.splitext(arquivo)
+         extensao = Path(caminho_arquivo).suffix
          print(f"Arquivo: {arquivo} | Extensão: {extensao}")
-         os.makedirs(extensao[1:], exist_ok=True)
+         pasta = f'{caminho}/{extensao[1:]}'
+         os.makedirs(pasta, exist_ok=True)
 
+def mover(caminho):
+   try:
+      arquivos = os.listdir(caminho)
+   except FileNotFoundError:
+      print(f"Pasta '{caminho}' não encontrada.")
+      return
 
+   for arquivo in arquivos:
+      caminho_arquivo = os.path.join(caminho, arquivo)
+      if os.path.isfile(caminho_arquivo):
+         extensao = Path(caminho_arquivo).suffix
+         print(f"Arquivo: {arquivo} | Extensão: {extensao}")
+         pasta = f'{caminho}/{extensao[1:]}'
+         shutil.move(caminho_arquivo, f'{pasta}/{arquivo}')
+         
 if __name__ == "__main__":
-   pasta_alvo = "./teste"  # Crie essa pasta e coloque arquivos lá para testar
-   listar_arquivos_e_extensoes(pasta_alvo)
+   pasta_alvo = "./testes"  # Crie essa pasta e coloque arquivos lá para testar
+   criar_pastas(pasta_alvo)
+   mover(pasta_alvo)
